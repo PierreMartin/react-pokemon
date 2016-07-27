634139af4bd56d76fd4259e158f2c37851270e4c
Pierre Martin <pierremartin.pro@gmail.com>
2016-07-27 23:57:21 +0200

    add infos dev

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                         <div className='col-sm-12 text-center'>
                             <h3 className='lead'><strong>Information</strong></h3>
                             <p>Propulsé par <strong>Node.js</strong>, <strong>MongoDB</strong>, <strong>React</strong> et <strong>Flux</strong> (pour l'architecture + le rendu coté serveur).</p>
-                            <p>Le code source est disponible sur <a href='https://github.com/' target="_blank">Github</a></p>
+                            <p>Le code source est disponible sur <a href='https://github.com/PierreMartin/react-pokemon' target="_blank">Github</a></p>
                             <p>© 2016 Pierre Mrt.</p>
                         </div>
                     </div>
---
+++ readme.md
@@ -0,0 +1,7 @@
+# React Pokemon Rating App
+
+Propulsé par Node.js, MongoDB, React et Flux (pour l'architecture + le rendu coté serveur).
+
+Démo : http://51.254.120.16/pokemon/
+
+© 2016 Pierre Mrt.
